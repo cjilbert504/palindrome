57769f1e5e6e856f06a6c7f1f0fb6836582999bd
Collin Jilbert <cjilbert504@gmail.com>
2020-05-30 09:15:28 -0500

    Set up as NPM module

--- index.js
+++ index.js
@@ -1,43 +1,19 @@
+module.exports = Phrase;
+
 // Reverses a string.
-function reverse(string) {
-    return Array.from(string).reverse().join("");
+String.prototype.reverse = function reverse() {
+    return Array.from(this).reverse().join("");
 }
 
 function Phrase(content) {
     this.content = content;
 
-    this.processor = function(string) {
-        return string.toLowerCase();
-    }
-
     this.processedContent = function processedContent() {
-        return this.processor(this.content);
+        return this.content.toLowerCase();
     }
 
     this.palindrome = function palindrome() {
-        return this.processedContent() === reverse(this.processedContent());
-    }
-
-    this.louder = function() {
-        return this.content.toUpperCase();
+        return this.processedContent() === this.processedContent().reverse();
     }
 }
 
-function TranslatedPhrase(content, translation) {
-    this.content = content;
-    this.translation = translation;
-
-    this.processedContent = function processedContent() {
-    return this.processor(this.translation);
-  }
-}
-
-TranslatedPhrase.prototype = new Phrase();
-
-String.prototype.blank = function blank() {
-    return !!(this.match(/^\s*$/));
-}
-
-Array.prototype.last = function() {
-    return this.slice(-1)[0];
-}
