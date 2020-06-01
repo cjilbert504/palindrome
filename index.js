b6ac3547b046e5b4a3998a043107f2c98ccb2873
Collin Jilbert <cjilbert504@gmail.com>
2020-05-31 19:20:24 -0500

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -9,11 +9,16 @@ function Phrase(content) {
     this.content = content;
 
     this.processedContent = function processedContent() {
-        return this.content.toLowerCase();
+        return this.letters().toLowerCase();
     }
 
     this.palindrome = function palindrome() {
         return this.processedContent() === this.processedContent().reverse();
     }
+
+    this.letters = function letters() {
+        return (this.content.match(/[a-z]/gi) || []).join("");
+    }
 }
+
 
